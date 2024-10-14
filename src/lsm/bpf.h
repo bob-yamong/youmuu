bc974ae7e72494382696c3592125de4f1adfaf62
Yejune Ko <57734106+KoYejune0302@users.noreply.github.com>
2024-10-14 11:57:54 +0900

    refactor: Update bpf.h

--- src/lsm/bpf.h
+++ src/lsm/bpf.h
@@ -1,12 +1,12 @@
 #ifndef __BPF_H
 #define __BPF_H
 
-#include "vmlinux.h"
-#include "shared.h"
-
 #include <bpf/bpf_core_read.h>
 #include <bpf/bpf_helpers.h>
 #include <bpf/bpf_tracing.h>
+
+#include "vmlinux.h"
+#include "shared.h"
 
 struct {
     __uint(type, BPF_MAP_TYPE_RINGBUF);
