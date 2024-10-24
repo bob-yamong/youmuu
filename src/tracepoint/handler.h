822d442943103e3c68cbce701e0b354035b9b6e5
Changhyun Lee <dlckdgus200011@gmail.com>
2024-10-24 19:06:07 +0900

    refactor: replace swith-case with array indexing

--- src/tracepoint/handler.h
+++ src/tracepoint/handler.h
@@ -12,7 +12,6 @@
 #include "structs.h"
 #include "event.h"
 
-void get_task_info_str(const struct current_task *task, char *buf, size_t buf_size);
 int handle_event(void *ctx, void *data, size_t data_sz);
 
 #endif
