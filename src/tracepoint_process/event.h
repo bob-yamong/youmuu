cf46041146df4a9f2f49b8818c9a6f38fa54db3f
Sunghyun Jeon <nukunga123@gmail.com>
2024-11-03 15:47:44 +0900

    Refactor event.h: Remove commented out code and trailing whitespace

--- src/tracepoint_process/event.h
+++ src/tracepoint_process/event.h
@@ -23,22 +23,5 @@ struct event {
     __u64 cgroup_id;
     char cgroup_name[MAX_CGROUP_NAME_LEN];  // cgroup 이름 추가
 }__attribute__((packed));
-// struct event {
-//     __u64 timestamp;
-//     __u64 cnt;
-//     __u32 pid;
-//     __u32 tid;
-//     __u32 ppid;
-//     __u32 uid;
-//     __u32 syscall_nr;
-//     char comm[TASK_COMM_LEN];
-//     char syscall[16];
-//     __u64 args[6];
-//     char filename[MAX_FILENAME_LEN];
-//     char argv[MAX_ARGS][MAX_ARG_LEN];
-//     __u64 cgroup_id;
-//     char cgroup_name[MAX_CGROUP_NAME_LEN];  // cgroup 이름 추가
-// };__attribute__((packed));
-
 
 #endif // EVENT_H
