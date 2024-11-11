25deb28214615c1a47fdf510c6f9a184babfd0dd
Sunghyun Jeon <nukunga123@gmail.com>
2024-11-12 00:55:21 +0900

    feat: update YamlPolicy and YamlContainerPolicy structures to include api_version and name for better organization

--- src/lsm/parser.h
+++ src/lsm/parser.h
@@ -98,15 +98,15 @@ struct YamlTracepointPolicy {
 
 struct YamlContainerPolicy {
     string container_name;
+    bool raw_tp_policy;
     YamlLsmPolicy lsm_policies;
     YamlTracepointPolicy tracepoint_policy;
 };
 
 struct YamlPolicy {
-    string container_name;
-    bool raw_tp_policy;
-    YamlLsmPolicy lsm_policies;
-    YamlTracepointPolicy tracepoint_policy;
+    string api_version;
+    string name;
+    vector<YamlContainerPolicy> containers;
 };
 
 // IP 주소 문자열을 정수로 변환하는 함수
