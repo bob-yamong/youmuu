5a7dbe037e42d9c33520a6b2c90b07675355f70e
Changhyun Lee <dlckdgus200011@gmail.com>
2024-11-11 18:15:25 +0900

    refactor: add db.h buffer.h user_struct.h

--- src/tracepoint/handler.h
+++ src/tracepoint/handler.h
@@ -10,8 +10,12 @@
 #include <sys/sysinfo.h>
 #include <asm/unistd_64.h>
 #include "struct.h"
+#include "db.h"
+#include "buffer.h"
+#include "user_struct.h"
 
 extern time_t boot_time;
+extern EventBuffer event_buffer;
 
 int handle_event(void *ctx, void *data, size_t data_sz);
 void init_event_handlers(void);
