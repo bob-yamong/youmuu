445e8c8c771533f8542084fc36d0f12b12ff4642
Yejune Ko <koyejune0302@gmail.com>
2024-11-21 12:23:49 +0000

    refactor: Add debug logging for policy counts in should_monitor function

--- src/lsm/bpf_map_structs.h
+++ src/lsm/bpf_map_structs.h
@@ -61,12 +61,15 @@ static __always_inline int should_monitor(struct task_struct *task, enum policy_
 
     switch (type) {
         case POLICY_FILE: {
+            bpf_printk("file policy num:%d from pid_ns_id: %u, mnt_ns_id: %u\n", value->num_file_policies, key.pid_ns_id, key.mnt_ns_id);
             if (value->num_file_policies > 0) return 1;
         }
         case POLICY_NETWORK: {
+            bpf_printk("network policy num:%d from pid_ns_id: %u, mnt_ns_id: %u\n", value->num_network_policies, key.pid_ns_id, key.mnt_ns_id);
             if (value->num_network_policies > 0) return 1;
         }
         case POLICY_PROCESS: {
+            bpf_printk("process policy num:%d from pid_ns_id: %u, mnt_ns_id: %u\n", value->num_process_policies, key.pid_ns_id, key.mnt_ns_id);
             if (value->num_process_policies > 0) return 1;
         }
     }
