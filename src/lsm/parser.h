7fffb485297b8e6b6b589ab04be59c331325161a
Sunghyun Jeon <nukunga123@gmail.com>
2024-12-06 02:33:18 +0900

    feat: add container info management and YAML policy structures

--- src/lsm/parser.h
+++ src/lsm/parser.h
@@ -1,11 +1,13 @@
-#ifndef POLICY_PARSER_H
-#define POLICY_PARSER_H
+#pragma once
+#ifndef PARSER_H
+#define PARSER_H
 
 #include <iostream>
 #include <string>
 #include <vector>
 #include <map>
 #include <asm/unistd_64.h>
+#include <arpa/inet.h>
 
 #include "../include/reflect-cpp/include/rfl/thirdparty/yyjson.h"
 #include "../include/reflect-cpp/src/yyjson.c"
@@ -13,9 +15,16 @@
 #include "../include/reflect-cpp/src/reflectcpp.cpp"
 #include "../include/reflect-cpp/include/rfl/yaml.hpp"
 
-using namespace std;
+#include "yaml_structs.h"
 
-map<string, __u32> flags_map = {
+struct DockerEventData {
+    std::mutex* mtx;
+    std::condition_variable* cv;
+    std::string buffer; // 수신된 데이터를 누적할 버퍼
+};
+
+
+std::map<std::string, __u32> flags_map = {
     {"POLICY_AUDIT", POLICY_AUDIT},
     {"POLICY_DENY", POLICY_DENY},
     {"POLICY_ALLOW", POLICY_ALLOW},
@@ -37,7 +46,7 @@ map<string, __u32> flags_map = {
     {"POLICY_PROC_SETUID", POLICY_PROC_SETUID}
 };
 
-__u32 string_to_flags(vector<string> str_flags){
+__u32 string_to_flags(std::vector<std::string> str_flags){
     __u32 flags = 0;
 
     for (const auto& str_flag : str_flags) {
@@ -48,8 +57,8 @@ __u32 string_to_flags(vector<string> str_flags){
 }
 
 
-vector<string> flags_to_string(__u32 flags){
-    vector<string> flags_str;
+std::vector<std::string> flags_to_string(__u32 flags){
+    std::vector<std::string> flags_str;
 
     for (const auto& flag : flags_map) {
         if (flags & flag.second) {
@@ -66,61 +75,18 @@ struct IpAddress {
     uint32_t subnet_mask;
 };
 
-// Policy 관련 구조체 정의
-struct YamlNetworkPolicy {
-    string ip;
-    int port;
-    int protocol;
-    vector<string> flags;
-    vector<int> uid;
-};
-
-struct YamlFilePolicy {
-    string path;
-    vector<string> flags;
-    vector<int> uid;
-};
-
-struct YamlProcessPolicy {
-    string comm;
-    vector<string> flags;
-    vector<int> uid;
-};
-
-struct YamlLsmPolicy {
-    vector<YamlFilePolicy> file;
-    vector<YamlNetworkPolicy> network;
-    vector<YamlProcessPolicy> process;
-};
-
-struct YamlTracepointPolicy {
-    vector<string> syscalls;
-};
-
-struct YamlContainerPolicy {
-    string container_name;
-    bool raw_tp_policy;
-    YamlLsmPolicy lsm_policies;
-    YamlTracepointPolicy tracepoint_policy;
-};
-
-struct YamlPolicy {
-    string api_version;
-    string name;
-    vector<YamlContainerPolicy> containers;
-};
 
 // IP 주소 문자열을 정수로 변환하는 함수
-IpAddress parse_ip(const string& ip_str) {
+IpAddress parse_ip(const std::string& ip_str) {
     IpAddress result = {0, 0};
     
     size_t slash_pos = ip_str.find('/');
-    string ip_part = ip_str.substr(0, slash_pos);
+    std::string ip_part = ip_str.substr(0, slash_pos);
     
     inet_pton(AF_INET, ip_part.c_str(), &result.ip);
     result.ip = ntohl(result.ip);
     
-    if (slash_pos != string::npos) {
+    if (slash_pos != std::string::npos) {
         int prefix_len = stoi(ip_str.substr(slash_pos + 1));
         if (prefix_len >= 0 && prefix_len <= 32) {
             result.subnet_mask = prefix_len == 0 ? 0 : (~0U << (32 - prefix_len));
